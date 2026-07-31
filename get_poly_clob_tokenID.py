98c5865d980ee27fd4d2c8424c583fb1e728a30a
brad-hof <bradhofmeister1@gmail.com>
2026-07-31 12:43:07 -0700

    Updated get_poly_clob_tokenID.py script

--- get_poly_clob_tokenID.py
+++ get_poly_clob_tokenID.py
@@ -9,7 +9,7 @@ pd.set_option('display.max_rows', None)
 
 if __name__ == "__main__":
     tag_id = get_poly_tag_id(['mlb'])
-    rows = fetch_poly_markets(tag_id, '2026-07-29')
+    rows = fetch_poly_markets(tag_id, 'YYYY-MM-DD')
     final_df = poly_df_and_clobIDs(rows)
     print(final_df)
 
